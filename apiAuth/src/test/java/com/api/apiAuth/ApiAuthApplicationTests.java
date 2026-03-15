18163be3dce53f12b12c884fe90249c1b7b22b2c
Pablo Nieto Hurtado <pnietohurtado03@gmail.com>
2026-03-15 20:42:56 +0100

    First commit

---
+++ apiAuth/src/test/java/com/api/apiAuth/ApiAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.apiAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
